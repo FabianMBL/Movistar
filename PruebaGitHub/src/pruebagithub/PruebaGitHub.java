d5945bed857130390f168566a73912eeb1a5c817
ivancs99 <ivancs99@github.com>
2020-01-17 12:06:42 +0100

    Update PruebaGitHub.java

--- PruebaGitHub/src/pruebagithub/PruebaGitHub.java
+++ PruebaGitHub/src/pruebagithub/PruebaGitHub.java
@@ -12,6 +12,7 @@ public class PruebaGitHub {
     
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Fabi eres un gilipollas");
     }
 
 }
